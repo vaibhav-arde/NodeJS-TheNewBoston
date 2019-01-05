d75cf6b6fe0a8ce44211ba1ea8a33ba981a572a0
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-06 02:38:49 +0530

    Added code of 5th Video to understand reference to object

---
+++ V05-ReferenceToObj.js
@@ -0,0 +1,13 @@
+let VaibhaV = {
+    favFood: "Warana",
+    favDrink : "Lassi"
+}
+
+let Sheetal = VaibhaV;
+
+console.log(VaibhaV);
+console.log(Sheetal);
+
+Sheetal.favFood ="Bharit";
+console.log(VaibhaV);
+console.log(Sheetal);

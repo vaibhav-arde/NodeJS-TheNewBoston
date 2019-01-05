aa7f9535ac899c78424908cb8ccfd5c4e1ae2d52
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-06 01:28:22 +0530

    Notes from third session

---
+++ Basic3.js
@@ -0,0 +1,26 @@
+var person ={
+    firstName : "VaibhaV",
+    lastName : "Arde",
+    age : 32
+}
+
+console.log(person);
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(3, 7));
+
+function worthLess(){
+
+}
+console.log(worthLess());
+
+var anyVar = function(){
+    console.log("Veg is proties rich , dont belive doctors");
+}
+
+anyVar();
+
+setTimeout(anyVar, 5000);

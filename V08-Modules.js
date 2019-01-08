0e1505d7134d3ef618233099a3f963281cdef55d
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-08 10:15:53 +0530

    Updated V08 and added V09 for modules

--- V08-Modules.js
+++ V08-Modules.js
@@ -1,3 +1,6 @@
-const movies = require('./movies');
+const movies08 = require('./V08-Movies');
+const movies09 = require('./V09-Movies');
 
-movies.Avatar();
+movies08.Avatar();
+movies09.printAvatar();
+console.log(movies09.myFavMovie);
---
+++ V09-Movies.js
@@ -0,0 +1,12 @@
+module.exports ={
+    printAvatar : function(){
+        console.log(`"Avatar : I like this Movie (from V09)"`);
+    },
+    
+    printChappie : function(){
+        console.log(`Chappie : I want to watch this Movie`);
+    },
+
+    myFavMovie : "The Matrix"
+};
+

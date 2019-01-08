8956d8d35fc79f9e142d13bb6beb1f3b9b3d6016
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-08 10:18:12 +0530

    deleted movies.js

--- movies.js
+++
@@ -1,9 +0,0 @@
-function printAvatar(){
-    console.log(`"Avatar : I like this Movie"`);
-}
-
-function printChappie(){
-    console.log(`Chappie : I want to watch this Movie`);
-}
-
-module.exports.Avatar = printAvatar;

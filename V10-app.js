b63bcbabedb1eec66f60c740e88e075f2b46ff74
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-08 10:42:37 +0530

    Shared state of module

--- V10-app.js
+++ V10-app.js
@@ -1,2 +1,8 @@
 require('./V10-Sheetal');
 require('./V10-VaibhaV');
+//Shared state of module"
+/* Though VaibhaV's favMovie is not defined it is printing value from Sheetal's favMovie this 
+is not bug but good for performance in chat applications
+Sheetal's favorite movie is "Bhai : Vyekti or Valli"
+VaibhaV's favorite movie is "Bhai : Vyekti or Valli"
+*/

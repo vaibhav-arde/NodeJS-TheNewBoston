a171489aa25493a4c762418018a1d15d40612c91
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-09 22:43:26 +0530

    V11 Object factory

--- V11-app.js
+++ V11-app.js
@@ -1,2 +1,9 @@
 require('./V11-Sheetal.js');
 require('./V11-VaibhaV.js');
+
+//Object Factory"
+/* to present VaibhaV's and Sheetals favorite movies differently use object factory
+Create function which returns set of values, and use that different modules.
+Sheetal's favorite movie is : "Bhai : Vyakti or Valli"
+VaibhaV's favorite movie is : ""
+*/

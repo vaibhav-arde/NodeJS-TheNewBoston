b7d6ab5dcc69858024faaa749628095639a6b57c
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-15 20:57:18 +0530

    V13 creating server

--- V13-server.js
+++ V13-server.js
@@ -3,7 +3,8 @@ var http = require('http');
 function firstRequest(request, response){
     console.log(`User made a request ${request.url}`);
     response.writeHead(200, {"Context-Type":"text/plain"});
-    response.write("");
+    response.write("Here is some data");
+    response.end();
 }
 
 http.createServer(firstRequest).listen(8888);

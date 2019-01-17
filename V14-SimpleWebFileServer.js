be162c3f7d697acd77ff7c038a1d55aa52f7dd34
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-17 21:02:42 +0530

    V14 Simple Web Server

--- V14-SimpleWebFileServer.js
+++ V14-SimpleWebFileServer.js
@@ -1,8 +1,24 @@
 var http = require('http');
+var fs =require('fs');
 
-function webFileServer(request, response){
-    
+//404 response
+function send404Response(response){
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write(`Error 404 : Page not found!`);
+    response.end();
 }
 
+//Handle user request
+function webFileServer(request, response){
+    if(request.method == 'GET' && request.url =='/'){
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        fs.createReadStream("./V14-index.html").pipe(response);
+    }
+    else{
+        send404Response(response);
+    }
+}
+
+//Handle a user request
 http.createServer(webFileServer).listen(8888);
 console.log(`Starting a WebFileServer`);

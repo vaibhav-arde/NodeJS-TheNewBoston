c9ba8048c6aa33f20d49f2a4c239adf3ccca1fea
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-24 17:48:41 +0530

    installed express

--- V16-Express.js
+++ V16-Express.js
@@ -1,6 +1,8 @@
 //Create a folder (myExpress), navigate to that path and install using : npm install -g express-generator
-//then : express myexpress
-//npm install
-//npm start
+//then create a new directory using express : express myexpress
+//Move to new directory using cd command.
+//install npm using npm command : npm install
+//Write following command in terminal in order to run express application in vs code : npm start
+//Open browser with url : http://localhost:3000
 //backdated date
 //Test
